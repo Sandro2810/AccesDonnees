51a67bfa6b63998e84b6d8023adf085e1f1eb929
sandro2810 <sandro.cleto@he-arc.ch>
2021-12-21 16:30:47 +0100

    Initial commit

---
+++ src/main/java/hearc/heg/tb/tpmulitmodaux/App.java
@@ -0,0 +1,13 @@
+package hearc.heg.tb.tpmulitmodaux;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
